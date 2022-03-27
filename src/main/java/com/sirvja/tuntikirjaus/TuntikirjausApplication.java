6cd3305aea32839e20e5c7b34f8a6f0a425eb95b
Janne Sirviö <janne.sirvio@bcbmedical.com>
2022-03-27 21:52:48 +0300

    Add table row to change color on selection

--- src/main/java/com/sirvja/tuntikirjaus/TuntikirjausApplication.java
+++ src/main/java/com/sirvja/tuntikirjaus/TuntikirjausApplication.java
@@ -4,11 +4,13 @@ import javafx.application.Application;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Cursor;
 import javafx.scene.Scene;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 
 import java.io.IOException;
 
@@ -22,6 +24,7 @@ public class TuntikirjausApplication extends Application {
         Scene scene = new Scene(fxmlLoader.load(), 900, 600);
         scene.getStylesheets().add(String.valueOf(TuntikirjausApplication.class.getResource("main-view_dark.css")));
         stage.setScene(scene);
+        stage.setTitle("Tuntikirjaus App");
         stage.show();
     }
 
