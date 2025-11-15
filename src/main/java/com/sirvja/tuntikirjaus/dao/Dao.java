909ed74e8486475758701b8383ba5ca0b8cc690e
Janne Sirviö <jaausi@outlook.com>
2025-11-15 11:57:20 +0200

    Update daos to support different type of primary keys

--- src/main/java/com/sirvja/tuntikirjaus/dao/Dao.java
+++ src/main/java/com/sirvja/tuntikirjaus/dao/Dao.java
@@ -6,13 +6,15 @@ import java.time.LocalDate;
 import java.util.List;
 import java.util.Optional;
 
-public interface Dao<T> {
+public interface Dao<T, I> {
 
-    Optional<T> get(int id);
+    Optional<T> get(I id);
 
     @Deprecated(since = "1.0.2")
-        // Use getAllToList instead
-    Optional<ObservableList<T>> getAll();
+    // Use getAllToList instead
+    default Optional<ObservableList<T>> getAll() {
+        throw new RuntimeException("Used method is deprecated, use getAllToList() instead");
+    }
 
     List<T> getAllToList();
     List<T> getAllFromToList(LocalDate localDate);
